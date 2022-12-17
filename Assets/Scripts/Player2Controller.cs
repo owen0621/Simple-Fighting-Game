2bd667d136716820c1ee87c2901c6c0b9c1c3b03
owen0621 <owen10270936@gmail.com>
2022-12-17 16:44:40 +0800

    add player2

--- Assets/Scripts/Player2Controller.cs
+++ Assets/Scripts/Player2Controller.cs
@@ -5,25 +5,91 @@ using UnityEngine;
 public class Player2Controller : MonoBehaviour
 {
     private playerState curstate = playerState.idle;
+    public float speed = 1f;
+    Vector3 left = new Vector3(0.3f, -0.3f, 0);
+    private Animator animator;
+    private int count = 0;
+    private int delay = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        animator.SetInteger("state", 0);
+        if (curstate != playerState.idle)
+        {
+            if (count < delay)
+            {
+                ++count;
+            }
+            else
+            {
+                curstate = playerState.idle;
+                count = 0;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.L) && this.transform.position == left)
+            {
+                curstate = playerState.move;
+                this.transform.position += new Vector3(speed, 0f, 0f);
+                delay = 100;
+            }
+            else if (Input.GetKeyDown(KeyCode.J) && this.transform.position != left)
+            {
+                curstate = playerState.move;
+                this.transform.position += new Vector3(-speed, 0f, 0f);
+                delay = 100;
+            }
+            else if (Input.GetKeyDown(KeyCode.U))
+            {
+                curstate = playerState.kick;
+                animator.SetInteger("state", 1);
+                delay = 300;
+            }
+            else if (Input.GetKeyDown(KeyCode.O))
+            {
+                curstate = playerState.ukick;
+                animator.SetInteger("state", 2);
+                delay = 250;
+            }
+            else if (Input.GetKeyDown(KeyCode.M))
+            {
+                curstate = playerState.defence;
+                animator.SetInteger("state", 3);
+                delay = 250;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        PlayerController player = collision.GetComponent<PlayerController>();
-        if (collision != null)
+        PlayerController other = collision.GetComponent<PlayerController>();
+        playerState otherState = other.getCurState();
+        Debug.Log(otherState);
+        if (curstate == playerState.kick || curstate == playerState.ukick)
         {
-            playerState other = player.getCurState();
-            Debug.Log(other);
+            if (otherState == playerState.kick || otherState == playerState.ukick)
+            {
+                curstate = playerState.hurt;
+                animator.SetInteger("state", 4);
+                delay = 400;
+            }
+
+        }
+        else if (curstate == playerState.defence)
+        {
+        }
+        else
+        {
+            curstate = playerState.hurt;
+            animator.SetInteger("state", 4);
+            delay = 400;
         }
     }
 
@@ -36,4 +102,58 @@ public class Player2Controller : MonoBehaviour
     {
         curstate = newState;
     }
+
+    //private playerState curstate = playerState.idle;
+    //public float speed = 0.1f;
+    //Vector3 right = new Vector3(-0.3f, -0.3f, 0);
+    //private Animator animator;
+    //private int count = 0;
+    //private int delay = 0;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    Player2Controller other = collision.GetComponent<Player2Controller>();
+    //    playerState otherState = other.getCurState();
+    //    Debug.Log(otherState);
+    //    if (curstate == playerState.kick || curstate == playerState.ukick)
+    //    {
+    //        if (otherState == playerState.kick || otherState == playerState.ukick)
+    //        {
+    //            curstate = playerState.hurt;
+    //            animator.SetInteger("state", 3);
+    //            delay = 400;
+    //        }
+
+    //    }
+    //    else if (curstate == playerState.defence)
+    //    {
+    //    }
+    //    else
+    //    {
+    //        curstate = playerState.hurt;
+    //        animator.SetInteger("state", 3);
+    //        delay = 400;
+    //    }
+    //}
+
+    //public playerState getCurState()
+    //{
+    //    return curstate;
+    //}
+
+    //public void setCurState(playerState newState)
+    //{
+    //    curstate = newState;
+    //}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
 {
 
     private playerState curstate = playerState.idle;
-    public float speed = 0.1f;
+    public float speed = 1f;
     Vector3 right = new Vector3(-0.3f, -0.3f, 0);
     private Animator animator;
     private int count = 0;
@@ -61,34 +61,34 @@ public class PlayerController : MonoBehaviour
             {
                 curstate = playerState.kick;
                 animator.SetInteger("state", 1);
-                delay = 300;
+                delay = 400;
             }
             else if (Input.GetKeyDown(KeyCode.G))
             {
                 curstate = playerState.ukick;
                 animator.SetInteger("state", 2);
-                delay = 250;
+                delay = 300;
             }
             else if (Input.GetKeyDown(KeyCode.R))
             {
                 curstate = playerState.defence;
                 animator.SetInteger("state", 3);
-                delay = 250;
+                delay = 300;
             }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Player2Controller player = collision.GetComponent<Player2Controller>();
-        playerState otherState = player.getCurState();
+        Player2Controller other = collision.GetComponent<Player2Controller>();
+        playerState otherState = other.getCurState();
         Debug.Log(otherState);
         if (curstate == playerState.kick || curstate == playerState.ukick)
         {
             if (otherState == playerState.kick || otherState == playerState.ukick)
             {
                 curstate = playerState.hurt;
-                animator.SetInteger("state", 3);
+                animator.SetInteger("state", 4);
                 delay = 400;
             }
 
@@ -99,7 +99,7 @@ public class PlayerController : MonoBehaviour
         else
         {
             curstate = playerState.hurt;
-            animator.SetInteger("state", 3);
+            animator.SetInteger("state", 4);
             delay = 400;
         }
     }
